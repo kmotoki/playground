7a09b67afd7c9e32804035a47ce358a44f0c610a
kiyoshi.motoki <kiyoshi.motoki@gmail.com>
2017-04-25 16:21:49 +0900

    refs #9

---
+++ chef-repo/site-cookbooks/playground-web/attributes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: playground-web
+# Attributes:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default['playground-web']['cake_dir'] = '/usr/local/playground'
